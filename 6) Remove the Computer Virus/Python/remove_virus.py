5e947c8b12eafbb7c79769895a3af75401aae94b
Gabriel Dornelas <zerick.gd@gmail.com>
2025-05-24 22:05:00 +0000

    refactor: updates virus challenge

--- 6) Remove the Computer Virus/Python/remove_virus.py
+++ 6) Remove the Computer Virus/Python/remove_virus.py
@@ -1,4 +1,13 @@
-def remove_virus(pc_files_str):
+def remove_virus(pc_files_str: list[str]) -> str:
+    '''Finds the viruses strings in pc_files_str and removes them.
+    Bad files will contain "virus" or "malware", but "antivirus" and "notvirus" will not be viruses.
+
+    Args:
+        pc_files_str: The input list of strings with PC Files: + each file in the PC.
+
+    Returns:
+        list[str]: List with PC Files: Empty" if there are no files left on the computer or PC Files: + safe files.
+    '''
     files_list = pc_files_str.split(': ')[1].split(', ')
     cleaned_list = []
 
@@ -15,7 +24,12 @@ def remove_virus(pc_files_str):
     return answer
 
 if __name__ == '__main__':
-    print(remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg"))
-    print(remove_virus("PC Files: antivirus.exe, cat.pdf, lethalmalware.exe, dangerousvirus.exe"))
-    print(remove_virus("PC Files: notvirus.exe, funnycat.gif"))
-    print(remove_virus("PC Files: virus.exe, lethalmalware.exe, dangerousvirus.bat"))
+    func = remove_virus
+    inputs = [
+        "PC Files: spotifysetup.exe, virus.exe, dog.jpg",
+        "PC Files: antivirus.exe, cat.pdf, lethalmalware.exe, dangerousvirus.exe",
+        "PC Files: notvirus.exe, funnycat.gif",
+        "PC Files: virus.exe, lethalmalware.exe, dangerousvirus.bat"
+    ]
+    for item in inputs:
+        print(f"{item}: {func(item)}")
